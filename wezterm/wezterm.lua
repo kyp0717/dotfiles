08834f27bcac855c7b547c5303d5a739b124aba4
Phage Ky <kyp0717@gmail.com>
2025-04-15 15:24:18 -0400

    add wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,35 +1,61 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.default_prog = { 'nu' }
+config.default_prog = { "/home/kip/.cargo/bin/nu" }
 
-config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono'
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 
 config.font_size = 16
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 
-config.window_background_opacity = 0.88
+config.window_background_opacity = 1.0
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 config.window_background_gradient = {
-  interpolation = 'Linear',
+	interpolation = "Linear",
 
-  orientation = 'Vertical',
+	orientation = "Vertical",
 
-  blend = 'Rgb',
+	blend = "Rgb",
 
-  colors = {
-    '#11111b',
-    '#181825',
-  },
+	colors = {
+		"#11111b",
+		"#181825",
+	},
 }
 
 config.use_fancy_tab_bar = false
+
+config.leader = { key = "a", mods = "CTRL" }
+config.keys = {
+	{
+		key = "h",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{ key = "K", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{
+		key = "l",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.ReloadConfiguration,
+	},
+}
 
 return config
