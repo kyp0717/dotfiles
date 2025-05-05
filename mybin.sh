1763b352c1d2e842228e5843ef063d064fd2509f
Phage Ky <karbon.0717@google.com>
2025-05-05 14:19:38 -0400

    add eza

--- mybin.sh
+++ mybin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! command -v cargo &>/dev/null; then
-    echo "Cargo is not installed. Aborting."
+    echo "Cargo is not installed. Installing now."
     curl https://sh.rustup.rs -sSf | sh
 fi
 
-cargo install ripgrep
+cargo install ripgrep eza
