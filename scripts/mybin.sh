0c6bec0fbadca58eea533be8420aa20f8ee2871f
Phage Ky <kyp0717@gmail.com>
2025-07-18 12:47:35 -0400

    add wezterm

--- scripts/mybin.sh
+++ scripts/mybin.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+sudo apt install -y gnome-tweaks curl 
+sudo apt install -y pkg-config libssl-dev build-essential
+sudo apt install -y lua5.4 opam carapace-bin xclip wl-clipboard zoxide
+sudo apt install -y wezterm
+
 if ! command -v cargo &>/dev/null; then
     echo "Cargo is not installed. Installing now."
     curl https://sh.rustup.rs -sSf | sh
 fi
 
 cargo install ripgrep eza bat
-sudo apt install lua5.4 opam carapace-bin xclip wl-clipboard atuin zoxide
+cargo install nu --locked
+sudo apt install -y lua5.4 opam carapace-bin xclip wl-clipboard atuin zoxide
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install -y neovim
 
 ## install nerdfont
 ## ensure that ~/.local/bin is in your path
@@ -14,6 +23,3 @@ curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
 ## intall atuin
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
-## ocaml setup
-opam init -y
-opam install ocaml-lsp-server odoc ocamlformat utop
