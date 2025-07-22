9aca405d394412a242ff59997b6fbc42c7d2099b
Phage Ky <karbon.0717@google.com>
2025-07-22 08:35:09 -0400

    add auto-read to neovim

--- nvim-mod/.config/nvim/lua/options.lua
+++ nvim-mod/.config/nvim/lua/options.lua
@@ -77,3 +77,9 @@ vim.opt.foldlevel = 20
 -- vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()' -- Example using treesitter
 vim.opt.foldexpr = 'nvim_treesitter.foldexpr()' -- Example using treesitter
 -- vim: ts=2 sts=2 sw=2 et
+--
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'VimResume', 'CursorHold' }, {
+  pattern = '*',
+  command = 'checktime',
+})
